3dd8064135c19ca4235b39e64ca332af2e6da19d
jaro1411 <jaredobare91@gmail.com>
2022-07-04 06:48:29 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
+/**
+*main - prints the size of various types
+*Return: 0 if exited properly, non-zero otherwise
+*/ 
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
