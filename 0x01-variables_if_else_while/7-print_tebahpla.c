69cbc6a3c97ca2f00db78440446d743822799b01
jaro1411 <jaredobare91@gmail.com>
2022-07-04 20:43:26 -0700

    i

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * main - Entry point
+ */
+
+int main(void)
+
+{
+int low;
+for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+putchar('\n');
+return (0);
+}
