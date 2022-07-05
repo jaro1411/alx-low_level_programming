39a85617a2549a74f9d3aa7287ac585e1b301a02
jaro1411 <jaredobare91@gmail.com>
2022-07-04 20:31:31 -0700

    o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+{
+	if (low != 'e' && low != 'q')
+		putchar(low);
+}
+putchar('\n');
+return (0);
+}
