88813761414160de43033a9330d8bc5f5bd042eb
jaro1411 <jaredobare91@gmail.com>
2022-07-06 02:56:24 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main entry point of a the progam
+ * Return: always 0
+ */
+
+void print_alphabet(void);
+{
+int a;
+for (a = 'a',a <= 'z', j++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
