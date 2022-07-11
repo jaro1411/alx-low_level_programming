96b161ca917cff9100eb6bdf1b8612e292e74e73
jaro1411 <jaredobare91@gmail.com>
2022-07-11 08:41:15 -0700

    4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-/**                                                                             *main - prints the size of various types                                        *Return: 0 if exited propere
+/**
+*main - Entry point
+*Return: Always 0 (Success)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
+
 return (0);
 }
