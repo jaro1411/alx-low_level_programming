68d9b5050a74f10da45f373e2a09473066d9f23e
jaro1411 <94676583+jaro1411@users.noreply.github.com>
2022-07-12 23:52:29 +0300

    a function that prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
