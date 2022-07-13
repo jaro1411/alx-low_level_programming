288abf17ca69d51fcaebeb52d02ec097678b9a22
jaro1411 <94676583+jaro1411@users.noreply.github.com>
2022-07-13 11:55:10 +0300

    void reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset var
+ * @n : the argument
+ * Description: print out result
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
