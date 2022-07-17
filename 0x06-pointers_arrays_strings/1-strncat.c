f770f0cb4ceaaaba8a180928663dde93dd6616e3
jaro1411 <jaredobare91@gmail.com>
2022-07-17 04:47:50 -0700

    strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncat - a function that concatenates two strings
+ *@dest: first parameter
+ *@src: second parameter
+ *@n: number of bytes
+ *Return: a string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
+
+
