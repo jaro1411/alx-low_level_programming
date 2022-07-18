a3e4b864a5b1d18bb50da0e34fb391956b03f9a3
jaro1411 <jaredobare91@gmail.com>
2022-07-17 21:21:37 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * string_toupper: converting any string to upper case
+ * @x: string param
+ * Return: string
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+		{
+			x[i] -= 32;
+		}
+	i++;
+}
+}
+
+
+
+
