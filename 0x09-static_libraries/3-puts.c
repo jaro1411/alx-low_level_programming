d36f5ae14d3c591f85bf7f2ce0217c8a12eb925a
jaro1411 <94676583+jaro1411@users.noreply.github.com>
2022-07-25 20:32:11 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,14 @@
-void _puts(char *s)
+#include "main.h"
+
+/**
+* _puts -> this is a function that puts
+* @str: a param to _puts function
+*/
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
