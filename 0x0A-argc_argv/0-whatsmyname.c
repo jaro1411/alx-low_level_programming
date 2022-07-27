225ce6f3dd282fa1cd29c395f2ed59a033eefc3d
jaro1411 <jaredobare91@gmail.com>
2022-07-26 20:39:42 -0700

    prints the mname of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the name of the program
+ * @argc: count of the arguments
+ * @argv: the individual arguments to the main
+ * Return: 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/REAME.md
@@ -0,0 +1,1 @@
+This is all about arguments to the main
