69fbc1c8f595c693f9c89052dd9eb275603f4119
jaro1411 <jaredobare91@gmail.com>
2022-08-09 03:15:21 -0700

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
+ *
  * Description: Longer description
  */
 
@@ -15,15 +16,4 @@ struct dog
 	float age;
 	char *owner;
 }
-
-/**
- *dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 #endif
